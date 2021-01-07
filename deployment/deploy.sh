43f89a151c3c36c268ce55ae152b3dd685372aaa
Anastas Stoyanovsky <astoyan@us.ibm.com>
2021-01-07 16:43:56 -0500

    Pass GPG passphrase to ./gradlew build

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+
   # upload artifacts
   ./gradlew -Psigning.password=$PASSPHRASE uploadArchives
 fi

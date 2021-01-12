8bc484cabf046a5f70c7afcf8c8fdeb105e94bfb
Anastas Stoyanovsky <contact@anastas.eu>
2021-01-12 11:19:41 -0500

    Update deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'main' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 
   # upload artifacts
   ./gradlew -Psigning.password=$PASSPHRASE uploadArchives
